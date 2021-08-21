69b3967b6c079ca0d21d47c1f830657245c2d885
A01026608 <89270787+A01026608@users.noreply.github.com>
2021-08-21 18:23:24 -0500

    Update exercise.py
    operaciones básicas

--- assignments/01OperacionesBasicas/src/exercise.py
+++ assignments/01OperacionesBasicas/src/exercise.py
@@ -1,7 +1,16 @@
 def main():
     #escribe tu código abajo de esta línea
-    pass
 
+a=int(input('Dame un número:'))
+b=int(input('Dame un número:'))
+
+suma= a+b
+resta= a-b
+multi= a*b
+
+print('Suma:',suma)
+print('Resta:',resta)
+print('Multiplicación:', multi)
 
 if __name__ == '__main__':
     main()
