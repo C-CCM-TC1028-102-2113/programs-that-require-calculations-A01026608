60d484fc6de7b3d4f3fdd114bcf45115997ade2f
A01026608 <89270787+A01026608@users.noreply.github.com>
2021-08-21 20:11:51 -0500

    Update exercise.py

--- assignments/04CalculaTiempo/src/exercise.py
+++ assignments/04CalculaTiempo/src/exercise.py
@@ -1,6 +1,11 @@
 def main():
     #escribe tu código abajo de esta línea
-    pass
+    edad= int(input('Dame tu edad:'))
+    año_actual= int(input('Dame el año actual:'))
+    
+    año_final= (año_actual - edad) +100
+    print('Cumplirás 100 años en el año:', año_final)
+   
 
 
 
